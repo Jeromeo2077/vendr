4c3fde4505b08c9a066c341743ba9562fdce49e6
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-27 14:47:17 -0600

    Fixed import for the AppState.js

--- app/AppState.js
+++ app/AppState.js
@@ -1,3 +1,4 @@
+import { Snack } from './models/Snacks.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
@@ -5,13 +6,13 @@ class ObservableAppState extends EventEmitter {
 
 
   snacks = [
-    new Snack({ name: 'Soda', price: 1.50, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' })
-  new Snack({ name: 'Chips', price: 2.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' })
-  new Snack({ name: 'Cookies', price: 3.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' })
-  new Snack({ name: 'Jerky', price: 4.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' })
-  new Snack({ name: 'Eggs', price: 2.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' })
-  new Snack({ name: 'Cheese', price: 2.50, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' })
-  new Snack({ name: 'Bacon', price: 4.50, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' })
+    new Snack({ name: 'Soda', price: 1.50, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' }),
+    new Snack({ name: 'Chips', price: 2.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' }),
+    new Snack({ name: 'Cookies', price: 3.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' }),
+    new Snack({ name: 'Jerky', price: 4.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' }),
+    new Snack({ name: 'Eggs', price: 2.00, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' }),
+    new Snack({ name: 'Cheese', price: 2.50, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' }),
+    new Snack({ name: 'Bacon', price: 4.50, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png' }),
   ]
 
   money = 0
