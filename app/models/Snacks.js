176a77c016528b80ea7901ddfe7a15d7631132c0
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-27 15:36:34 -0600

    Updated the formatting on the index.html, Snack Model HTML and style.css

--- app/models/Snacks.js
+++ app/models/Snacks.js
@@ -11,7 +11,7 @@ export class Snack {
 
   get HTMLSnackButtonTemplate() {
     return `
-      <button class="btn btn-outline-dark fs-1">Jerky</button>
+      <button class="btn btn-success m-3 snackButton"><p>${this.name}</p><p>${this.price}</p></button>
         `
   }
 
